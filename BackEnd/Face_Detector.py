a0b3b3b113bfc75d5f04cbe57ab57bf5d96b6cfa
Giri-Sudip <83629986+Giri-Sudip@users.noreply.github.com>
2022-05-27 11:10:13 +0545

    updated codes

--- BackEnd/Face_Detector.py
+++ BackEnd/Face_Detector.py
@@ -1,48 +1,90 @@
-from tkinter import image_names
 import cv2
 import urllib.request
 import numpy as np
 
-# load some pre-trained data on face frontals from opencv using algorithm
-trained_face_data = cv2.CascadeClassifier(
-    'haarcascade_frontalface_default.xml')
 
 
-url='http://127.0.0.1:8000/images/'
-url_response = urllib.request.urlopen(url)
-# # bytes_data = uploaded_file.getvalue()
+
+url = [
+    "http://127.0.0.1:8000/images/"
+]
 
 
-#  choosing image to detect face
-img = cv2.imread(url_response)
+def url_to_image(url):
+    # download the image, convert it to a NumPy array, and then read it into OpenCV format
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    #return the image
+    return image
 
 
-# # converting color to gray scale.
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-facecascade = cv2.CascadeClassifier('http://127.0.0.1:8000/images/')
+for url in url:
+    trained_face_data = cv2.CascadeClassifier(
+        'haarcascade_frontalface_default.xml')
+    x = y = w = h = int
+    image = url_to_image(url)
+    face_coordinates = trained_face_data.detectMultiScale(image,
+                                                          scaleFactor=1.1,
+                                                          minNeighbors=5,
+                                                          minSize=(30, 30),
+                                                          flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
 
 
-# # Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
-# # Draw rectangles
-# # cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
-cv2.rectangle(img, (315, 431), (315+1286, 431+1286), (0, 255, 0), 2)
-
-img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
-img = cv2.imdecode(img_array, -1)
-
-# # Display Grey Image
-cv2.imshow('Face Detection', img)
 
 
-print(face_coordinates)
 
 
-# # wait for execution
-cv2.waitKey()
+# from tkinter import image_names
+# import cv2
+# import urllib.request
+# import numpy as np
 
-print("successfully done")
+# # load some pre-trained data on face frontals from opencv using algorithm
+# trained_face_data = cv2.CascadeClassifier(
+#     'haarcascade_frontalface_default.xml')
+
+
+# url='http://127.0.0.1:8000/images/'
+# url_response = urllib.request.urlopen(url)
+# # # bytes_data = uploaded_file.getvalue()
+
+
+# #  choosing image to detect face
+# img = cv2.imread(url_response)
+
+
+# # # converting color to gray scale.
+# grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# facecascade = cv2.CascadeClassifier('http://127.0.0.1:8000/images/')
+
+
+# # # Detect faces
+# face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# # # Draw rectangles
+# # # cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+# cv2.rectangle(img, (315, 431), (315+1286, 431+1286), (0, 255, 0), 2)
+
+# img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+# img = cv2.imdecode(img_array, -1)
+
+# # # Display Grey Image
+# cv2.imshow('Face Detection', img)
+
+
+# print(face_coordinates)
+
+
+# # # wait for execution
+# cv2.waitKey()
+
+# print("successfully done")
 
 
 # import numpy as np
